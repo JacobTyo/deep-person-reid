5d3838cf688e9d4e08312b19aeea5ee0f60aca5b
Jake Tyo <jacob.tyo@gmail.com>
2024-01-18 15:09:17 -0500

    more slurm tools

---
+++ slurm_launcher_single.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -p zack_reserved
+#SBATCH -t 72:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=50G
+#SBATCH -c 10
+#SBATCH --output=/home/jtyo/slurm_output/%x.%j.out
+#SBATCH --error=/home/jtyo/slurm_output/R-%x.%j.err
+
+module load singularity
+
+singularity exec --nv /home/jtyo/containers/deep-person-reid.sif bash -c '
+    # Load conda and activate the environment
+    . /opt/conda/etc/profile.d/conda.sh
+    conda activate base
+
+    # Change to the appropriate directory
+    cd /home/jtyo/Repos/deep-person-reid
+
+    # update python path
+    export PYTHONPATH=/home/jtyo/Repos/deep-person-reid
+
+    # Run script with given config
+    python scripts/CMIL_main.py --config $1
+'
